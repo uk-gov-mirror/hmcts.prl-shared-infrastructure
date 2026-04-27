d6dece428f14903fd640486b89bc46ce0c13e552
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-04-27 11:24:14 +0100

    add jenkins mi access

--- main.tf
+++ main.tf
@@ -11,12 +11,18 @@ module "key-vault" {
   env                 = var.env
   tenant_id           = var.tenant_id
   object_id           = var.jenkins_AAD_objectId
+  jenkins_object_id   = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_platformengineering group object ID
   product_group_name      = "DTS Family Private Law"
   common_tags             = var.common_tags
   create_managed_identity = true
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 data "azurerm_key_vault" "key_vault" {
