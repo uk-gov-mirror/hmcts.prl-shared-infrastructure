2d3e32cd0c37245ada7e350f607f9a6835c95bb0
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-04-27 12:48:16 +0100

    use for_each

--- import.tf
+++ import.tf
@@ -1,4 +1,5 @@
 import {
-  to = azurerm_key_vault_secret.prl_pcq_token_key
-  id = var.token_key_import
+  for_each = contains(["ithc", "perftest", "demo"], var.env) ? [1] : []
+  to       = azurerm_key_vault_secret.prl_pcq_token_key
+  id       = var.token_key_import
 }
